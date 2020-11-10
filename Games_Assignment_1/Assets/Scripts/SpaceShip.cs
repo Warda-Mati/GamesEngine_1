0b097e138367cf8080cf8ecf30209fb694817089
Warda-Mati <46999666+Warda-Mati@users.noreply.github.com>
2020-11-10 16:42:17 +0000

    movement

---
+++ Games_Assignment_1/Assets/Scripts/SpaceShip.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShip : MonoBehaviour
+{
+    public int speed;
+
+    public int rotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vert = Input.GetAxis("Vertical");
+        float horiz = Input.GetAxis("Horizontal");
+        
+        transform.Translate(0,0,speed*Time.deltaTime * vert);
+        transform.Rotate(rotationSpeed*Time.deltaTime * horiz, 0, 0);
+        
+        
+
+    }
+}
