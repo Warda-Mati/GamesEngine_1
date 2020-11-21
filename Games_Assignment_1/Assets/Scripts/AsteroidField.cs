01609375727eca8167c7c8f60cdd8d23fb43e53a
Warda-Mati <46999666+Warda-Mati@users.noreply.github.com>
2020-11-21 16:47:18 +0000

    created asteroid

---
+++ Games_Assignment_1/Assets/Scripts/AsteroidField.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidField : MonoBehaviour
+{
+    public GameObject[] asteroids;
+    public int numberOfAsteroids;
+    public int width,height;
+
+    private float areaOfField;
+    // Start is called before the first frame update
+    void Start()
+    {
+        areaOfField = width * height;
+        for (int i = 0; i < numberOfAsteroids; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-areaOfField,areaOfField),Random.Range(-areaOfField,areaOfField),Random.Range(-areaOfField,areaOfField));
+            Vector3 finalPos = transform.TransformPoint(pos);
+            GameObject asteroid = GameObject.Instantiate(asteroids[Random.Range(0,3)],pos,Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
