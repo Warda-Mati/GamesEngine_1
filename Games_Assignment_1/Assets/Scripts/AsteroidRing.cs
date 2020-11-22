172d8eac0aa12072ccb04ebc58261c74e98c7692
Warda-Mati <46999666+Warda-Mati@users.noreply.github.com>
2020-11-22 16:56:15 +0000

    added asteroidRing

---
+++ Games_Assignment_1/Assets/Scripts/AsteroidRing.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidRing : MonoBehaviour
+{
+    public int numOfAsteroids;
+    public int radius;
+
+    public GameObject asteroid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < numOfAsteroids; i++)
+        {
+            float angle = Mathf.PI * 2  / numOfAsteroids * i;
+            float x = Mathf.Sin(angle) * radius ;
+            float y = Mathf.Cos(angle) * radius;
+            Vector3 pos = new Vector3(x, 0, y);
+            pos = transform.TransformPoint(pos);
+            for (int j = 0; j < 4; j++)
+            {
+                Vector3 space = new Vector3(Random.Range(-5f,5f),Random.Range(-5f,5f),Random.Range(-5f,5f));
+                GameObject newAsteroid = Instantiate(asteroid, pos + space, Quaternion.identity);
+                newAsteroid.transform.parent = this.transform;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
