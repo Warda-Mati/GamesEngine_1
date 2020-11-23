247b7351b3c3d2cdf519212203323065a0b81f3b
Warda-Mati <46999666+Warda-Mati@users.noreply.github.com>
2020-11-23 17:10:55 +0000

    added spaceship

---
+++ Games_Assignment_1/Assets/Scripts/AlienSpaceship.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienSpaceship : MonoBehaviour
+{
+    public int NumOfCircles;
+
+    public float radius;
+
+    public float size;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < NumOfCircles; i++)
+        {
+            float angle = Mathf.PI * 2  / NumOfCircles * i;
+            float x = Mathf.Sin(angle) * radius ;
+            float y = Mathf.Cos(angle) * radius;
+            Vector3 pos = new Vector3(x, 0, y);
+            pos = transform.TransformPoint(pos);
+            GameObject circles = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            circles.transform.position = pos;
+            circles.transform.localScale = new Vector3(size,size,size);
+            circles.transform.parent = transform;
+
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        
+    }
+}
