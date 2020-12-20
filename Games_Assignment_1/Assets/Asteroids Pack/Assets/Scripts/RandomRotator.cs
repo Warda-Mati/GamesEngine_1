40e9d37ccb722bfab8abc912c0d48e638bbeb3c8
Warda-Mati <46999666+Warda-Mati@users.noreply.github.com>
2020-12-20 13:29:13 +0000

    Can shoot asteroids

--- Games_Assignment_1/Assets/Asteroids Pack/Assets/Scripts/RandomRotator.cs
+++ Games_Assignment_1/Assets/Asteroids Pack/Assets/Scripts/RandomRotator.cs
@@ -1,13 +1,33 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
+using System.Security.Cryptography;
+using Random = UnityEngine.Random;
 
 public class RandomRotator : MonoBehaviour
 {
     [SerializeField]
     private float tumble;
-
+    
     void Start()
     {
         GetComponent<Rigidbody>().angularVelocity = Random.insideUnitSphere * tumble;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("bullet"))
+        {
+            Destroy(other.gameObject);
+            for (int i = 0; i < 3; i++)
+            {
+                
+                GameObject asteroid = Instantiate(gameObject,transform.position,transform.rotation);
+                asteroid.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+                asteroid.GetComponent<Rigidbody>().velocity = new Vector3(Random.Range(0.5f,1.5f),Random.Range(0.5f,1.5f),Random.Range(0.5f,1.5f));
+            }
+
+            Destroy(this.gameObject);
+        }
     }
 }
