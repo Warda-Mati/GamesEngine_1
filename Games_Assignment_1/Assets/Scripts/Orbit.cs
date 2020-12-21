8dd2022022cdbf8399fa2a04211d6b4ebdfac61e
Warda-Mati <46999666+Warda-Mati@users.noreply.github.com>
2020-12-21 12:29:44 +0000

    Added planets

---
+++ Games_Assignment_1/Assets/Scripts/Orbit.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    public GameObject[] planets;
+    public int distance;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < planets.Length; i++)
+        {
+            GameObject newPlanet = Instantiate(planets[i]);
+            Vector3 pos = new Vector3(distance*i,0,0);
+            newPlanet.transform.position = transform.TransformPoint(pos);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
