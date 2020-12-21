afb092844ba5ed25f03a60bb5d65a0882c04e120
Warda-Mati <46999666+Warda-Mati@users.noreply.github.com>
2020-12-21 17:58:43 +0000

    made progress on ring

--- Games_Assignment_1/Assets/Scripts/PlanetRings.cs
+++ Games_Assignment_1/Assets/Scripts/PlanetRings.cs
@@ -7,18 +7,20 @@ public class PlanetRings : MonoBehaviour
     public int segments;
 
     public float radius, thickness;
-    
+
+    public Material Material;
     // Start is called before the first frame update
     void Start()
     {
         GameObject rings = new GameObject("Planet Ring");
         rings.transform.parent = transform;
-        rings.transform.position = transform.TransformPoint(transform.position);
+        rings.transform.localPosition = Vector3.zero;
         rings.transform.rotation = Quaternion.identity;
 
         MeshFilter filter = rings.AddComponent<MeshFilter>();
         Mesh mesh = filter.mesh;
         MeshRenderer renderer = rings.AddComponent<MeshRenderer>();
+        renderer.material = Material; 
         
         Vector3[] vertices = new Vector3[(segments + 1)*2*2];
         int[] triangles = new int[segments * 6 * 2];
@@ -27,27 +29,27 @@ public class PlanetRings : MonoBehaviour
 
         for (int i = 0; i < segments; i++)
         {
-            float progress = i / segments;
+            float progress = (float)i / (float)segments;
             float angle = Mathf.Deg2Rad * progress * 360;
             float x = Mathf.Sin(angle);
             float y = Mathf.Cos(angle);
 
-            vertices[i * 2] = vertices[i * 2 + halfway] = new Vector3(x, 0, y) * (radius + thickness);
-            vertices[i * 2 + 1] = vertices[i * 2 + 1 + halfway] = new Vector3(x, 0, y) * (radius);
+            vertices[i * 2] = vertices[i * 2 + halfway] = new Vector3(x, 0f, y) * (radius + thickness);
+            vertices[i * 2 + 1] = vertices[i * 2 + 1 + halfway] = new Vector3(x, 0f, y) * (radius);
 
-            uv[i * 2] = uv[i * 2 + halfway] = new Vector2(progress, 0);
-            uv[i * 2 + 1] = uv[i * 2 + 1 + halfway] = new Vector2(progress, 1);
+            uv[i * 2] = uv[i * 2 + halfway] = new Vector2(progress, 0f);
+            uv[i * 2 + 1] = uv[i * 2 + 1 + halfway] = new Vector2(progress, 1f);
 
             if (i != segments)
             {
                 triangles[i * 12] = i * 2;
                 triangles[i * 12+1] = triangles[i * 12+4] = (i + 1)*2;
-                triangles[i * 12+2] = triangles[i * 12+3] = (i * 2)+1;
+                triangles[i * 12+2] = triangles[i * 12+3] = i * 2+1;
                 triangles[i * 12+5] = (i+1)*2+1;
                 
                 triangles[i * 12+6] = i * 2+halfway;
-                triangles[i * 12+7] = triangles[i * 12+10] = (i * 2)+1 + halfway;
-                triangles[i * 12+8] = triangles[i * 12+9] = (i +1)*2+halfway;
+                triangles[i * 12+7] = triangles[i * 12+10] = i * 2+1 + halfway;
+                triangles[i * 12+8] = triangles[i * 12+9] = i +1*2+halfway;
                 triangles[i * 12+11] = (i+1)*2+1+halfway;
                 
                 
