457ec063f9c49020b5c4dec506d05b5b6330e1e1
Warda-Mati <46999666+Warda-Mati@users.noreply.github.com>
2020-12-21 17:19:02 +0000

    started planet ring

---
+++ Games_Assignment_1/Assets/Scripts/PlanetRings.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetRings : MonoBehaviour
+{
+    public int segments;
+
+    public float radius, thickness;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject rings = new GameObject("Planet Ring");
+        rings.transform.parent = transform;
+        rings.transform.position = transform.TransformPoint(transform.position);
+        rings.transform.rotation = Quaternion.identity;
+
+        MeshFilter filter = rings.AddComponent<MeshFilter>();
+        Mesh mesh = filter.mesh;
+        MeshRenderer renderer = rings.AddComponent<MeshRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
