8c500a2bfd5fa8b7c3153ed9f6469a17a459f517
Warda-Mati <46999666+Warda-Mati@users.noreply.github.com>
2020-12-23 16:18:09 +0000

    added target system

--- Games_Assignment_1/Assets/Scripts/Shooter.cs
+++ Games_Assignment_1/Assets/Scripts/Shooter.cs
@@ -2,13 +2,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class Shooter : MonoBehaviour
 {
     public GameObject[] shooters = new GameObject[2];
 
     public GameObject bullets;
-
+    public RawImage targetSign;
     private int i;
     // Start is called before the first frame update
     void Start()
@@ -34,7 +36,10 @@ public class Shooter : MonoBehaviour
             if (Input.GetKey(KeyCode.K))
             {
                 Transform target = shooters[i].transform.GetChild(0);
-                GameObject bulletFired = Instantiate(bullets, target.transform.position, transform.rotation);
+                RectTransform targetT = targetSign.GetComponent<RectTransform>();
+                Vector3 targetPos = targetT.position - shooters[i].transform.position;
+                Quaternion targetRot = Quaternion.LookRotation(targetPos,Vector3.up);
+                GameObject bulletFired = Instantiate(bullets, target.transform.position, targetRot);
                 if (i == 0)
                 {
                     i = 1;
